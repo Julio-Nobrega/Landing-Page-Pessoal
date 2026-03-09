d1906bc5cade2d94a47f984707ef5ffbe2211c8a
Julio Nobrega <nobrega.jc30@gmail.com>
2026-03-09 20:04:44 -0300

    inserção de botões das aulas 6 e 7

--- js/script.js
+++ js/script.js
@@ -70,3 +70,101 @@ cardP.addEventListener("mouseout", function () {
 cardP.addEventListener("click", function () {
   cardP.style.backgroundColor = "rgb(250, 95, 95)";
 });
+
+/*const cardP = document.querySelector(".cardaula4");
+
+if (cardP) {
+  cardP.addEventListener("mouseover", function () {
+    cardP.style.backgroundColor = "rgb(134, 134, 241)";
+  });
+
+  cardP.addEventListener("mouseout", function () {
+    cardP.style.backgroundColor = "whitesmoke";
+  });
+
+  cardP.addEventListener("click", function () {
+    cardP.style.backgroundColor = "rgb(250, 95, 95)";
+  });
+}*/
+
+//AULA 5 -
+let contador = 1;
+//let do contador
+
+const botao = document.querySelector("#criarCard");
+
+const container = document.querySelector(".containerAula5");
+
+botao.addEventListener("click", function () {
+  //para cada click no notão
+  const novoCard = document.createElement("div");
+  //cria um elemento HTML (crio uma nova div onde adiciona o novo card)
+
+  novoCard.classList.add("cardNovo");
+  //adiciona classe no card novo
+
+  novoCard.textContent = "Card " + contador + "/ Clique para mudar de cor / Duplo clique para remover";
+  //adiciona texto dentro do card + 1 numero do contador
+
+  contador++;
+  //contador + 1
+
+  container.appendChild(novoCard);
+  //Ele entra dentro da div.
+
+  novoCard.addEventListener("click", function () {
+    novoCard.classList.toggle("cliquecard");
+  });
+  //ao clicar no card ele muda de cor
+
+  novoCard.addEventListener("dblclick", function () {
+    novoCard.remove();
+    //ao dar duplo clique o card é removido
+  });
+});
+
+//AULA 6
+const botaoFrutas = document.getElementById("btnFrutas");
+const frutas = document.getElementById("listaFrutas");
+
+botaoFrutas.addEventListener("click", function(){
+
+  if(frutas.classList.contains("escondido")){
+    frutas.classList.remove("escondido");
+  }else{
+    frutas.classList.add("escondido");
+  }
+});
+
+const botaoVerduras = document.getElementById("btnVerduras");
+const verduras = document.getElementById("listaVerduras");
+
+botaoVerduras.addEventListener("click", function(){
+  if(verduras.classList.contains("escondido")){
+    verduras.classList.remove("escondido");
+  }
+  else{
+    verduras.classList.add("escondido");
+  }
+});
+
+//AULA 7
+
+const botaoCarnes = document.getElementById("btnCarnes");
+const carnes = document.getElementById("listaCarnes");
+
+botaoCarnes.addEventListener("click", function(){
+  molhos.classList.add("escondido");
+  carnes.classList.toggle("escondido");
+});
+
+const botaoMolhos = document.getElementById("btnMolhos");
+const molhos = document.getElementById("listaMolhos");
+
+botaoMolhos.addEventListener("click", function(){
+  carnes.classList.add("escondido");
+  molhos.classList.toggle("escondido");
+});
+
+
+
